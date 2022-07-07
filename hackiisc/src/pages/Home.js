48ae8547573a1e747e28fdcc28f91d0f04e54a9b
Ashutosh-codes <ashutoshgangwar312@gmail.com>
2022-07-07 15:16:03 +0530

    Added and Completed header.js

--- hackiisc/src/pages/Home.js
+++ hackiisc/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Header from "../components/Header/Header";
 // stylesheets
 import styles from "./Home.module.css";
 
 class Home extends React.Component {
     render(){
         return(
-            <>Homepage</>
+            <Header />
+            // <>Homepage</>
         );
     }
 }
