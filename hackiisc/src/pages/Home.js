e06250922ded85cf28e0a8cc110dcb3ecb552964
Ashutosh-codes <ashutoshgangwar312@gmail.com>
2022-07-09 23:00:49 +0530

    finishing touch to home and footer

--- hackiisc/src/pages/Home.js
+++ hackiisc/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import MediaCard from "../components/Cards/Cards";
-import image2 from "../images/image2.jpg";  
+import image2 from "../images/image2.jpg";
+import image1 from "../images/image1.jpg"  
 import Button from '@mui/material/Button';
 // stylesheets
 import styles from "./Home.module.css"
@@ -15,8 +16,11 @@ class Home extends React.Component {
           <Grid item xs={12} md={6}>
             <div className={styles.main} id="place-main-center">
             <h2>AI Model to find the efficiency of your employee</h2>
-          <h6>We help companies in finding the efficiency of their employees which help them to increase their productivity and can also select easily which user has to be given performance improvement plan.</h6>
-          <h6>An employee can also see where he or she stand in his or her company. They can check anytime in his branch or over all branches of the company globally, anytime through our evaluate performace tab given on the dashboard.</h6>
+          <h6>Meet Karmath, the cutting edge AI/ML software predicting your employees'
+efficiency! Know who is slacking, who is improving, and who deserves
+to be promoted! Data is presented in form of interactive graph, thus
+providing the company and the registered employee deep insights into
+their performance.</h6>
             </div>
           
           </Grid>
@@ -35,9 +39,9 @@ class Home extends React.Component {
         <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
               <MediaCard 
-                src={image2}
+                src={image1}
                 alt=""
-                text = "Use AI to find efficiency of your employee"
+                text = "Use AI to find where you stand"
               />
             </Grid>
           <Grid container item xs={12} md={6} >
