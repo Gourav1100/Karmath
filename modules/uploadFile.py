c5727b306ec05cf8477b74fc7fddba3e9759cc42
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-07-10 06:20:00 +0530

    Retry

--- modules/uploadFile.py
+++ modules/uploadFile.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from werkzeug.utils import secure_filename
 import os
 
@@ -17,3 +18,24 @@ def execute(request, UPLOAD_FOLDER):
         os.system("sh ./trainDEA")
         return "success"
     return "failed"
+=======
+from werkzeug.utils import secure_filename
+import os
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def execute(request, UPLOAD_FOLDER):
+    file = request.files['csv']
+    if file.filename == '':
+        return "No selected file"
+    elif file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        # file.save(os.path.join(UPLOAD_FOLDER, filename))      # use for multiple users
+        file.save(os.path.join(UPLOAD_FOLDER, 'sample.csv'))    # use for single user
+        os.system("sh ./trainDEA")
+        return "success"
+    return "failed"
+>>>>>>> 8c665c01457b4a78d3ed10cb4f9fdfb32b6d9ff3
