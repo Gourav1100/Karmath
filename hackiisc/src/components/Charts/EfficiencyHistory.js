b7515862cc53125de326f7dde49b27156381ab32
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-07-10 08:21:17 +0530

    Charts re added

--- hackiisc/src/components/Charts/EfficiencyHistory.js
+++ hackiisc/src/components/Charts/EfficiencyHistory.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { LineChart, Line, Legend, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
+import { LineChart, Line, Legend, CartesianGrid, Tooltip, XAxis, YAxis, ReferenceLine } from "recharts";
 
 const dateFormat = (tick) => {
     const dt = new Date(tick);
@@ -8,25 +8,17 @@ const dateFormat = (tick) => {
 }
 
 function EfficiencyHistory(props) {
-    let avg = [];
-    for (let i = 0; i < props.data.length; i++) {
-        let obj = {
-            date: props.data[i].date,
-            Average: props.avg
-        };
-        avg.push(obj);
-    }
+    let avgstr = "Average(" + props.data.avg + "%)";
     return (
         <LineChart width={730} height={250}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }} data={props.data.data}>
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis xAxisId={"data"} dataKey="date" tickFormatter={dateFormat} />
-            <XAxis xAxisId={"avg"} dataKey="date" hide={true} />
+            <XAxis dataKey="date" tickFormatter={dateFormat} />
             <YAxis />
             <Tooltip labelFormatter={dateFormat} />
             <Legend />
-            <Line xAxisId={"data"} type="monotone" data={props.data} dataKey="Efficiency" stroke="#8884d8" />
-            <Line xAxisId={"avg"} type="monotone" data={avg} dataKey="Average" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="Efficiency" stroke="#8884d8" />
+            <ReferenceLine y={props.data.avg} label={avgstr} />
         </LineChart>
     );
 }
