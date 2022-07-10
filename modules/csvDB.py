83ae68a72b33147c6d01fb311661a704f2a713f0
TejaswiBishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2022-07-10 06:02:22 +0530

    Database updated

--- modules/csvDB.py
+++ modules/csvDB.py
@@ -6,12 +6,19 @@ import sqlite3
 def execute(request):
     efficiency = pd.read_csv("./result.csv").iloc[:,12].values
     conn = sqlite3.connect('./db.db')
-    res = conn.execute(f"SELECT name, company FROM branch WHERE email='{request.email}'")
+    res = conn.execute(f"SELECT name, company FROM branch WHERE email='{request['email']}'")
     res = res.fetchone()
     table = res[1] + '_' + res[0]
-    conn.execute(f'CREATE TABLE IF NOT EXISTS "{table}" ("Efficiency"	INTEGER NOT NULL);')
+    conn.execute(f'CREATE TABLE IF NOT EXISTS "{table}" ("Efficiency"	REAL NOT NULL);')
     conn.commit()
-    conn.execute(f"DELETE * FROM {table}")
+    conn.execute(f"DELETE FROM {table}")
     conn.commit()
+    avg = 0
     for i in range(0, len(efficiency)):
         conn.execute(f"INSERT INTO {table} VALUES({efficiency[i] * 100})")
+        avg += efficiency[i] * 100
+    conn.commit()
+    avg /= efficiency
+    conn.execute(f"UPDATE {res[0]} SET average={avg}, nofusers={len(efficiency)}  WHERE email='{request.email}'")
+
+    
